bca4960f2d4d8c4a388ba91e8bfb4c49ef190efe
Konrud <konrud5@gmail.com>
2025-05-21 17:47:53 +0300

    feat(workflow): Setup AzureOpenAI and Create ReActAgent with appropriate prompt

--- backend/src/tools.py
+++ backend/src/tools.py
@@ -1,6 +1,8 @@
 import os
 from llama_index.core.workflow import Context, Event, HumanResponseEvent, InputRequiredEvent
 from llama_index.tools.tavily_research import TavilyToolSpec
+from llama_index.core.schema import Document
+from typing import List
 
 tavily_api_key = os.environ.get("TAVILY_API_KEY", "")
 
@@ -39,11 +41,21 @@ async def get_user_approve(ctx: Context) -> str:
         return "no"
 
 
-async def search_web(query: str) -> str:
-    """Search the web for the query and return the result."""
-    results = tavily_tool.search(query=query, max_results=1)
+async def search_web(query: str) -> List[Document] | str:
+    """Search the web for the query and return the result.
+    Args:
+        query: The query to search for.
+        
+        Returns:
+            results: A list of dictionaries containing the results:
+                url: The url of the result.
+                content: The content of the result.
+                
+                If no results are found, it returns "No results found.
+    """
+    results = tavily_tool.search(query=query, max_results=3)
     if results:
-        return results[0].text
+        return results
     else:
         return "No results found."
 
