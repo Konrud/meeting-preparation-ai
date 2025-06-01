135e7f8e2bc9128a2d2c9169e5a94483a2567f55
Konrud <konrud5@gmail.com>
2025-06-01 16:35:05 +0300

    feat(workflow.py): Create `get_calendar_data_step` and `calendar_data_parser_step`
    -**workflow.py**- Implementation for `get_calendar_data_step` and `calendar_data_parser_step` has not been finished yet.
    
    -**enums.py**- New value `CALENDAR_DATA_PARSER` has been added
    
    -**events.py**- New events have been added: `ProgressWorkflowStartEvent`, `CalendarDataRetrievalEvent`, and `CalendarDataParserEvent`. `ResearchEvent` has been added new properties (`attendees: List[str]` and `comany: str`)
    
    -**prompts.py**- New prompt templates have been added: `GET_CALENDAR_EVENTS_PROMPT_TEMPLATE` and `EXTRACT_CALENDAR_DATA_PROMPT_TEMPLATE`
    
    -**meeting_request.py**- `meeting_state.py` file name has been changed to `meeting_request.py` all data has also been changed accordingly

--- backend/src/enums.py
+++ backend/src/enums.py
@@ -4,6 +4,7 @@ from enum import Enum
 class ProgressEventType(str, Enum):
     INIT = "init"
     CALENDAR_DATA_RETRIEVAL = "calendar_data_retrieval"
+    CALENDAR_DATA_PARSER = "calendar_data_parser"
     PROCESSING = "processing"
     FORMATTING = "formatting"
     COMPLETED = "completed"
